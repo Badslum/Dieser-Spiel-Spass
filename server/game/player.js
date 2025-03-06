3c03eb9399a8c1e4383fb20f6acb1ecc5c0e2663
xMiriamLeixnerx <157234629+xMiriamLeixnerx@users.noreply.github.com>
2025-03-06 11:29:59 +0100

    Backend -> Main (#27)
    * Implementieren des Testingframeworks Mocha, Miriam Leixner; 06.03.2025
    
    ---------
    
    Co-authored-by: Badslum <badslum05@gmail.com>
    Co-authored-by: Badslum <162301804+Badslum@users.noreply.github.com>
    Co-authored-by: Badslum <bastianroth24@gmail.com>

--- server/game/player.js
+++ server/game/player.js
@@ -1,5 +1,114 @@
-// Is player alive?
-// Create teams (idealy team 1, 2, etc)
-// Create "tail" for player, evtl with length restrictions?
+// 
+// Description: Backend-functions to create player, move player and check if player is alive.
+//
+// Created by:      Miriam Leixner on 02.03.2025
+// Last updated by: Miriam Leixner on 04.03.2025
 //
 // ToDo for Miriam
+
+/*To Do!! verfolgen wann und wo eingebener Username und Spielerdaten gespeichert wird
+import username from "./../public/App.js"
+import playerid from "./../public/App.js"
+*/
+
+//definition of variables
+let player = {
+    name: "",
+    id: 0,
+    teamid: 0,
+    alive: true,
+    x: 0,
+    y: 0,
+    //To do define working tail  tail: [x,y]
+}
+let direction = ""
+
+//function createPLayer
+//To do: playerid und username aus App.js importieren
+//To do: do I need to assign a starting point(x,y) for the player?
+function createPlayer(name, playerid) {
+   return {
+        name: name,
+        id: playerid,
+        };
+}
+
+//function movePlayer
+function movePlayer(player, direction) {
+    switch (direction) {
+        case "up":
+            player.y += 1;
+            break;
+        case "down":
+            player.y -= 1;
+            break;
+        case "left":
+            player.x -= 1;
+            break;
+        case "right":
+            player.x += 1;
+            break;
+    }
+
+}
+
+// Create "tail" for player, evtl with length restrictions?
+//Tail starts at players territory and grows with each step the player takes
+//function createTail
+
+
+
+//function checkTeam
+function checkTeam(player){
+    if(player.teamid === 0){
+        team = 0
+        console.log("Team not yet defined")
+        elseif(player.teamid === 1);{
+            team = 1
+            console.log("Player belongs to team 1 (blue)")
+        }
+        elseif(player.teamid === 2);{
+            team = 2
+            console.log("Player belongs to team 2 (red)")
+        }
+    }
+    else(
+        console.log("Error: team not identified")
+    )
+}
+
+//function checkAlive
+function checkAlive(player) {
+    if (player.alive === false) {
+        console.log("Player is dead");
+    }
+}
+
+// Export the functions
+module.exports = {
+    createPlayer,
+    movePlayer
+};
+
+//Tests
+
+//Test for createPLayer
+const player1 = createPlayer(`testuser`, 123);
+console.log("Player name should be: testuser");
+console.log("Player name is:"  + player1.name);
+console.log("Player id should be: 123");
+console.log("Player id is: " + player1.id);
+
+//Test for movePlayer
+player.x = 0;
+player.y = 0;
+movePlayer(player, "up");
+movePlayer(player, "right");
+
+console.log("player position should be: 1, 1");
+console.log("player position is: " + `${player.x}, ${player.y}`);
+
+//Test for checkTeam
+player1.teamid = 2;
+checkTeam(player1);
+console.log("Player belongs to team 1 (blue)");
---
+++ server/game/team.js
@@ -0,0 +1,58 @@
+// 
+// Description: Backend-functions to create teams, 
+//
+// Created by:      Miriam Leixner on 02.03.2025
+// Last updated by: Miriam Leixner on 04.03.2025
+//
+
+//definition of variables
+let team = {
+    id: 0,
+    no_members: 0,
+    color: ""
+} 
+
+
+//function createTeam (and equal distribution of players)
+//function needs x to decide how many teams should be created (there should be so place in frontend where this is decided/defined)
+//function needs playerPool to know how many players are in the game (input needed)
+function createTeam(playerPool, x) {
+    let colors = ["blue", "red", "green", "yellow", "purple", "orange"];
+    let teams = [];
+    if (x >= 2){
+        let baseMembers = Math.floor(playerPool / x);
+        let remainingMembers = playerPool % x;
+
+        for (let i = 0; i < x; i++){
+            let team = {
+                id: i + 1,
+                no_members: baseMembers + (remainingMembers > 0 ? 1 : 0),
+                color: colors[i]
+            };
+            teams.push(team);
+            if (remainingMembers > 0) {
+                remainingMembers--;
+            }
+        }
+    }
+    return teams;
+}
+
+//To Do: Assign percententage of gameboard to each team? Varible needed?
+
+//Test for createTeam
+let playerPool = 10;
+let x = 3;
+let teams = createTeam(playerPool, x);
+
+console.log(`Team 1 ID: ${teams[0].id}`);
+console.log(`Team 1 Members: ${teams[0].no_members}`);
+console.log(`Team 1 Color: ${teams[0].color}`);
+
+console.log(`Team 2 ID: ${teams[1].id}`);
+console.log(`Team 2 Members: ${teams[1].no_members}`);
+console.log(`Team 2 Color: ${teams[1].color}`);
+
+console.log(`Team 3 ID: ${teams[2].id}`);
+console.log(`Team 3 Members: ${teams[2].no_members}`);
+console.log(`Team 3 Color: ${teams[2].color}`);
