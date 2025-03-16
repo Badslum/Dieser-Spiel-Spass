0df66fa076b0bfcc96c60c799ac03a9645476e5d
Chrisxx11 <weisbrodchristian@gmail.com>
2025-03-16 20:35:02 +0100

    Fix: Kommentar aktualisiert in gameState.test.js

--- test/gameState.test.js
+++ test/gameState.test.js
@@ -1,33 +1,29 @@
 // 
 // Description: Test check for Player amount, Test Check for % of Territories, Test gain Territory
 // 
-// ToDo for Christian Weissbrod
-
-//remove comment and insert own functions and varibles and adapt the example test to your needs
-
-
+// ToDo for Christian Weisbrod
 
 const gameState = require("../server/game/gameState");
 const gameBoard = Array.from({ length: 10 }, () => Array(10).fill(0));
 
-// Spieler zum gameState hinzufügen
+// Spieler hinzufügen
 gameState.addPlayer("player1", gameBoard);
 gameState.addPlayer("player2", gameBoard);
 gameState.addPlayer("player3", gameBoard);
 gameState.addPlayer("player4", gameBoard);
 
 // Spielflächen für Spieler setzen
-gameBoard[0][0] = 1; // player1
+gameBoard[0][0] = 1; 
 gameBoard[0][1] = 1; 
 gameBoard[1][0] = 1; 
 
-gameBoard[2][2] = 2; // player2
+gameBoard[2][2] = 2; 
 gameBoard[2][3] = 2; 
 gameBoard[3][2] = 2; 
 gameBoard[3][3] = 2; 
 gameBoard[4][4] = 2; 
 
-gameBoard[5][5] = 3; // player3
+gameBoard[5][5] = 3;
 gameBoard[5][6] = 3; 
 gameBoard[6][5] = 3; 
 gameBoard[6][6] = 3; 
@@ -35,7 +31,7 @@ gameBoard[7][7] = 3;
 gameBoard[8][8] = 3; 
 gameBoard[9][9] = 3; 
 
-gameBoard[4][0] = 4; // player4
+gameBoard[4][0] = 4;
 gameBoard[4][1] = 4; 
 gameBoard[4][2] = 4; 
 gameBoard[4][3] = 4; 
@@ -44,7 +40,7 @@ gameBoard[4][5] = 4;
 gameBoard[4][6] = 4; 
 gameBoard[4][7] = 4; 
 
-// Punktestände für alle Spieler berechnen
+// Punktestände berechnen
 ["player1", "player2", "player3", "player4"].forEach(playerId => gameState.updateScore(playerId));
 
 // Top 3 Spieler ausgeben
